a7505a5703521ec80d1c6221a70a43e569c504aa
Yan <yankrasny@gmail.com>
2014-04-23 08:10:36 -0400

    remove extra var

--- partitioning/partitioning.cpp
+++ partitioning/partitioning.cpp
@@ -33,7 +33,6 @@ void RepairDocumentPartition::getBaseFragmentsOneVersion(
 
 	BaseFragment frag;
 	for (size_t i = 0; i < offsets.size(); ++i) {
-		unsigned start = offsets[i];
 		for (size_t j = i + 1; j < offsets.size(); ++j) {
 			frag.start = offsets[i];
 			frag.end = offsets[j];
